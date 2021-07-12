6e11718b46131c66ce8271f5ddf7e126c7a3a756
shobhitdayal07 <85332232+shobhitdayal07@users.noreply.github.com>
2021-07-12 08:36:14 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -7,18 +7,21 @@ const router = express.Router()
 const auth = require('../middleware/auth_middleware')
 const cart_auth = require('../middleware/cartauth_middleware')
 
+// User Collection functionalities
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 
 
-router.post('/add', auth , productController.addProduct)
-router.delete('/delete', auth, productController.deleteProduct)
-router.get('/show', productController.showProduct)
-router.patch('/update',auth, productController.updateProduct)
+// Product Collection functionalities
+router.post('/add-product', auth , productController.addProduct)
+router.delete('/delete-product/:id', auth, productController.deleteProduct)
+router.get('/show-product', productController.showProduct)
+router.patch('/update-product/:id',auth, productController.updateProduct)
 
 
-router.patch('/addincart', cart_auth, cartController.addToCart)
-router.delete('/removefromcart', cart_auth, cartController.deleteFromCart)
+
+router.patch('/addto-cart/:id', cart_auth, cartController.addToCart)
+router.delete('/deletefrom-cart/:id', cart_auth, cartController.deleteFromCart)
 
 
 module.exports = router
